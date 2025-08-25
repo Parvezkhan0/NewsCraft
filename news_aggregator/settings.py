059c03919b519aa33f836a29b35abe8e59d9abc0
Parvez Khan <Parvezkhan02@outlook.com>
2025-08-25 20:35:34 +0530

    corrected Db URL

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host.strip()]
 
 # Application definition
 INSTALLED_APPS = [
